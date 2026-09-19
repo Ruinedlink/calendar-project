02a8b6c1e838583b56d5366e029e8d8f10ae61d2
AbelGarza08 <abelgarza2008@gmail.com>
2026-09-19 12:44:07 -0500

    didnt have code lmao

--- calendar_model.py
+++ calendar_model.py
@@ -1,1 +1,5 @@
-
+#calendar_model.py
+#calendar app back-end
+#This is the project, and it will be a calander built with QT.
+print("Patrick is so awsome, and abel is chud")
+print("I coded this in VS code and pushed it through the terminal")
